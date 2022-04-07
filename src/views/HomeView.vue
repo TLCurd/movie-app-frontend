982be49a39a7ee1d57c2cbcd190b711a6c511c72
TLCurd <TLCurdChiro@gmail.com>
2022-04-06 21:36:30 -0600

    add moviesIndex; v-for movie in movies

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,41 @@
+<script>import axios from "axios";
+
+export default {
+  data: function () {
+    return {
+      message: "Welcome to Vue.js!",
+      movies: [],
+    };
+  },
+  created: function () {
+    console.log("in create...")
+  },
+  methods: {
+    moviesIndex: function () {
+      console.log('In movies...');
+      axios.get(`http://localhost:3000/movies.json`).then(response => {
+        console.log(response.data);
+        this.movies = response.data;
+      });
+    }
+  },
+};
+</script>
+
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>{{ message }}</h1>
+    <!-- <p>{{ movies }}</p> -->
+
+    <button v-on:click="moviesIndex()">Show me allllll the movies</button>
+
+    <br />
+    <div v-for="movie in movies" v-bind:key="movie.id">
+      <br />
+      {{ movie.id }}. {{ movie.title }}
+      <p>Plot: {{ movie.plot }}</p>
+    </div>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
-</script>
+<style></style>
